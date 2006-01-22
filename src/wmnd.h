f1080a819f571475ae3af18b8447bdb79f89e2d7
Yuri D'Elia <wavexx@yuv.info>
2006-01-22 12:11:56 +0100

    Solaris 7 build fixes.
    [git-p4: depot-paths = "//depot/wmnd/main/": change = 1095]

--- src/wmnd.h
+++ src/wmnd.h
@@ -42,12 +42,11 @@
 
 #define MAXBUF 256
 
-#ifndef MAX
-#ifdef __GNUG__
-#define MAX(a, b) (a >? b)
-#else
-#define MAX(a, b) ((a < b)? b: a)
+#ifndef MIN
+#define MIN(a, b) (((a) < (b))? (a): (b))
 #endif
+#ifndef MAX
+#define MAX(a, b) (((a) < (b))? (b): (a))
 #endif
 
 
